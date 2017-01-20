dd0f7fa7b91c02a3bfc28e2d41cad70d70e0c0b9
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-19 20:59:27 -0800

    Rename application and headers

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <h1>Angular Boilerplate</h1>
+    <h1>Food Log</h1>
   </div>
   `
 })
